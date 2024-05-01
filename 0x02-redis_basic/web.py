1217d205c0542a7b90ad21fe9228c839859601a7
bneayoub <bne.ayoub@gmail.com>
2024-05-01 05:02:35 +0100

    Refactor caching functionality in web.py using Redis-py library

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
+'''A module with tools for request caching and tracking
 '''
 import redis
 import requests
@@ -7,32 +7,30 @@ from functools import wraps
 from typing import Callable
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
+def track_get_page(fn: Callable) -> Callable:
+    '''Decorator for get_page
     '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        '''Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
         '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
 
 
-@data_cacher
+@track_get_page
 def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
-    return requests.get(url).text
+    response = requests.get(url)
+    return response.text
